11bfa77a65056580c43e7da9c31be11fe744060d
马琪斌 <maqi1520@163.com>
2025-09-18 07:25:05 +0800

    fix: add axios

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const path = require("path");
 const express = require("express");
 const cors = require("cors");
 const morgan = require("morgan");
+const axios = require("axios");
 
 const logger = morgan("tiny");
 
